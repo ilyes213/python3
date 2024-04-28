2b6c46b2e7de200a1e200dd0e7f938ffdb22a618
ilyes213 <73720224+ilyes213@users.noreply.github.com>
2024-04-28 01:28:32 +0100

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,21 +1,22 @@
 import socket
 
-target_ip = input("أدخل عنوان الآي بي المستهدف: ")
+target_ip = input("الرجاء إدخال عنوان IP المستهدف: ")
 
-def البحث_عن_المنافذ_المفتوحة(ip):
-    print("جاري فحص المنافذ المفتوحة على", ip)
-    for port in range(1, 1025):
-        try:
+def اختراق_الهاتف_الذكي(ip):
+    try:
+        for port in range(1, 1025):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(0.5)
+            s.settimeout(1)
             result = s.connect_ex((ip, port))
             if result == 0:
                 print("المنفذ", port, "مفتوح")
-            s.close()
-        except KeyboardInterrupt:
-            print("\nتم إيقاف البحث.")
-            break
-        except Exception as e:
-            print("حدث خطأ:", e)
+                # هنا يمكنك إضافة رمز استغلال المنفذ المفتوح
+                break
+            else:
+                print("المنفذ", port, "مغلق")
+                # هنا يمكنك إضافة رمز البحث عن ثغرات والاستغلال المباشر للجهاز بالكامل
 
-البحث_عن_المنافذ_المفتوحة(target_ip)
+    except Exception as e:
+        print("حدث خطأ:", e)
+
+اختراق_الهاتف_الذكي(target_ip)
